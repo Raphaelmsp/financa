16e4f959e1a4328f140ba79207ac8b5ed2edd720
Raphaelmsp <raphaelmsp30@gmail.com>
2024-03-28 06:27:24 -0300

    feat:"configurar botao buscar"

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -12,12 +12,18 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
-  const { register, handleSubmit } = useForm<SearchFormInputs>({
+  const { 
+    register, 
+    handleSubmit,
+    formState: { isSubmitting } 
+  } = useForm<SearchFormInputs>({
     resolver: zodResolver(searchFormSchema),
   })
 
-  function handleSearchTransactions(data:  SearchFormInputs) {
-    console.log(data)
+  async function handleSearchTransactions(data:  SearchFormInputs) {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log(data);
   }
 
   return (
@@ -28,7 +34,7 @@ export function SearchForm() {
       {...register('query')}
     />
 
-      <button type="submit">
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20}/>
         Buscar
       </button>
