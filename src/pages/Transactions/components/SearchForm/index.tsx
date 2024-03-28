6a151a03bcc8587ee27431db8d38a93452f96d1b
Raphaelmsp <raphaelmsp30@gmail.com>
2024-03-28 04:54:40 -0300

    Update @hookform/resolvers to version 2.9.11 and add search form with input validation
    The package-lock.json and package.json files were updated to use the latest version of @hookform/resolvers (2.9.11). The SearchForm component in src/pages/Transactions/components/SearchForm/index.tsx was updated to use the useForm hook from react-hook-form and added input validation using Zod. The search form now has a single input field for querying transactions and a submit button with a MagnifyingGlass icon. The handleSearchTransactions function is currently empty and will be implemented in a future commit.

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,10 +1,28 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
+import * as z from 'zod';
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+  const { register, handleSubmit } = useForm<SearchFormInputs>()
+
+  function handleSearchTransactions() {
+
+  }
+
   return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input 
+      type="text" 
+      placeholder="Busque por transações" 
+      {...register('query')}
+    />
 
       <button type="submit">
         <MagnifyingGlass size={20}/>
