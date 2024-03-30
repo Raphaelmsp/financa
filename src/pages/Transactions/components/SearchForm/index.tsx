9cf51f99b43d2d42c5236096e121c2879e6016d9
Raphaelmsp <raphaelmsp30@gmail.com>
2024-03-30 00:20:40 -0300

    feat:"alterou o new promise por fetchTransactions(data.query) handleSearchTransactions no searchform"

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -25,9 +25,7 @@ export function SearchForm() {
   })
 
   async function handleSearchTransactions(data:  SearchFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-
-    console.log(data);
+    await fetchTransactions(data.query)
   }
 
   return (
